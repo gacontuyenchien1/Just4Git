f21972de95332a98ce9b9204e5858967d3fc23fc
TrongTran <gacontuyenchien1@gmail.com>
2020-05-15 00:59:44 +0700

    Trong add comment

--- app/src/main/java/com/example/just4git/MainActivity.java
+++ app/src/main/java/com/example/just4git/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("Hello World !!!");
 
         System.out.println("Trong changed!!!");
+        //Tai sao khong change duoc
     }
 }
